9901f67ea75ec0a3af73bf605888f12481cdd291
MoonJongHyeon1095 <109948801+MoonJongHyeon1095@users.noreply.github.com>
2022-12-16 18:01:18 +0900

    remove dist

--- dist/adapters/socket.adapter.d.ts
+++
@@ -1,4 +0,0 @@
-import { IoAdapter } from '@nestjs/platform-socket.io';
-export declare class SocketIoAdapter extends IoAdapter {
-    createIOServer(port: number, options?: any): any;
-}
--- dist/chat/dto/loginuser.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class LoginUserDto {
-    Id: string;
-    password: string;
-}
